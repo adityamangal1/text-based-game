653047ba1b5732fe4f62835cab1785ad0d3a3fc4
aditya mangal <adityamangal0202@gmail.com>
2021-10-11 12:14:20 +0530

    SWG Game

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from win32com.client import Dispatch
 import random
 from termcolor import cprint
+from time import sleep
 
 
 def game():
@@ -18,37 +19,49 @@ def game():
 
         if user == opp:
             print("Oops!!\nMatch draw")
+            sleep(1)
             print("You chooses", user, "And your opponent chooses", opp)
             chances += 1
 
         if user == 's' and opp == 'w':
+            print("You chooses", user, "And your opponent chooses", opp)
+            sleep(1)
             print("HURRAY!!")
             print("You win!")
-            print("You chooses", user, "And your opponent chooses", opp)
             user_win += 1
             chances += 1
+            sleep(1)
 
         elif user == 'w' and opp == 's':
+            sleep(1)
 
             print("You Loose!")
             print("\t\tDont worry, try again.")
             print("You chooses", user, "And your opponent chooses", opp)
+            sleep(1)
+
             chances += 1
             opp_win += 1
 
         if user == 'g' and opp == 'w':
+            sleep(1)
 
             print("You Loose!!")
             print("\t\tDont worry, try again.")
             print("You chooses", user, "And your opponent chooses", opp)
+            sleep(1)
+
             chances += 1
             opp_win += 1
 
         elif user == 'w' and opp == 'g':
+            sleep(1)
 
             print("HURRAY!!")
             print("You win!")
             print("You chooses", user, "And your opponent chooses", opp)
+            sleep(1)
+
             user_win += 1
             chances += 1
 
@@ -61,12 +74,16 @@ def game():
             opp_win += 1
 
         elif user == 'g' and opp == 's':
+            sleep(1)
+
             print("HURRAY!!")
             print("You win!")
             print("You chooses", user, "And your opponent chooses", opp)
+            sleep(1)
+
             user_win += 1
             chances += 1
-
+    sleep(1)
     print("Your final score is:-", user_win,
           "Your opponent score :-", opp_win)
     print("Hope you enjoyed!\nCome soon!!")
@@ -78,19 +95,23 @@ def again_play():
     play_again = input(
         "\t\t\tWant to play again?\nEnter 'y' for Yes and 'n' for No.\n")
     if play_again == 'y':
+        sleep(1)
         game()
     elif play_again == 'n':
+        sleep(1)
         print("\t\t*Thank You*!!\nCome Back Soon!")
     else:
         print("Wrong input.Please type 'y' or 'n' only.")
+        sleep(1)
         again_play()
 
 
 if __name__ == "__main__":
     adi = Dispatch("SAPI.spvoice")
-    adi.speak("This is a snake water gun game")
+    adi.speak("Welcome to snake water gun game")
     cprint("#" * 50, "magenta")
     cprint((f"SNAKE WATER GUN GAME ").center(50), "yellow")
     cprint("#" * 50, "magenta")
+    sleep(1)
     print("WELCOME!")
     game()
